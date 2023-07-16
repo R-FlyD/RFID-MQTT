49a13c5a8311a8b37d992eb3e37b1e90b43c4b5a
arthurcoelho442 <arthurcoelho442@gmail.com>
2023-07-16 09:59:15 -0300

    :white_check_mark: Finalizado

--- RFID-MQTT/RFID-MQTT.ino
+++ RFID-MQTT/RFID-MQTT.ino
@@ -4,11 +4,11 @@
 #include "RDM6300.h"
 
 //Parametros de conexão
-const char* ssid = "HomeRouter 2G"; // REDE
-const char* password = "1234ab1188@@##"; // SENHA
+const char* ssid = "Arthur"; // REDE
+const char* password = "12345678-ar"; // SENHA
 
 // MQTT Broker
-const char *mqtt_broker = "192.168.15.202";       //Host do broket
+const char *mqtt_broker = "192.168.1.132";       //Host do broket
 const char *topic = "RFID";                       //Topico a ser subscrito e publicado
 const char *mqtt_username = "";                   //Usuario
 const char *mqtt_password = "";                   //Senha
@@ -27,9 +27,9 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 
 //Inicializa a serial nos pinos 12 (RX) e 13 (TX) 
-SoftwareSerial RFID(D6, D7);
+SoftwareSerial RFID(D6, D8);
 
-RDM6300 RDM6300(Payload, D6, D7);
+RDM6300 RDM6300(Payload, D6, D8);
 
 //Prototipos
 bool connectMQTT();
@@ -45,21 +45,7 @@ void setup(void)
   //Inicializa a serial para o leitor RDM6300
   RFID.begin(9600);
   
-  // Conectar
-  WiFi.begin(ssid, password);
-
-  //Aguardando conexão
-  Serial.println();
-  Serial.print("Conectando");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
-
-  //Envia IP através da UART
-  Serial.println(WiFi.localIP());
+  reconectWiFi();
 
   mqttStatus =  connectMQTT();
 
@@ -68,10 +54,6 @@ void setup(void)
 }
 
 void loop() {if (mqttStatus){
-  delay(1000);
-  String temp = "{\"tag\": \"1F003F8A67\"}";
-  client.publish(topic, temp.c_str());
-
   //Apaga o led
   delay(100);
   digitalWrite(Led, HIGH);
@@ -94,11 +76,16 @@ void loop() {if (mqttStatus){
       Serial.println(tag);
     }
   }
+  //garante funcionamento das conexões WiFi e ao broker MQTT
+  VerificaConexoesWiFIEMQTT();
+
+  //keep-alive da comunicação com broker MQTT
   client.loop();
 }}
 
 bool connectMQTT() {
   client.setServer(mqtt_broker, mqtt_port);
+  client.setCallback(callback);
 
   int tentativa = 0;
   do {
@@ -122,4 +109,85 @@ bool connectMQTT() {
   if(tentativa >= 5)
     Serial.println("Não conectado");    
   return 0;
+}
+
+//Função: função de callback 
+//        esta função é chamada toda vez que uma informação de 
+//        um dos tópicos subescritos chega)
+//Parâmetros: nenhum
+//Retorno: nenhum
+void callback(char* topic, byte* payload, unsigned int length) 
+{
+    String msg;
+ 
+    //obtem a string do payload recebido
+    for(int i = 0; i < length; i++) 
+    {
+       char c = (char)payload[i];
+       msg += c;
+    }
+   
+    //toma ação dependendo da string recebida:
+    if (msg.equals("OI"))
+      Serial.println("OI");
+     
+}
+
+//Função: reconecta-se ao broker MQTT (caso ainda não esteja conectado ou em caso de a conexão cair)
+//        em caso de sucesso na conexão ou reconexão, o subscribe dos tópicos é refeito.
+//Parâmetros: nenhum
+//Retorno: nenhum
+void reconnectMQTT() 
+{
+    while (!client.connected()) 
+    {
+        Serial.print("* Tentando se conectar ao Broker MQTT: ");
+        Serial.println(mqtt_broker);
+        if (client.connect(client_id.c_str(), mqtt_username, mqtt_password)) {
+            Serial.println("Conectado com sucesso ao broker MQTT!");
+            client.subscribe(topic); 
+        } else{
+            Serial.println("Falha ao reconectar no broker.");
+            Serial.println("Havera nova tentatica de conexao em 2s");
+            delay(2000);
+        }
+    }
+}
+  
+//Função: reconecta-se ao WiFi
+//Parâmetros: nenhum
+//Retorno: nenhum
+void reconectWiFi() 
+{
+    //se já está conectado a rede WI-FI, nada é feito. 
+    //Caso contrário, são efetuadas tentativas de conexão
+    if (WiFi.status() == WL_CONNECTED)
+        return;
+         
+    WiFi.begin(ssid, password); // Conecta na rede WI-FI
+     
+    while (WiFi.status() != WL_CONNECTED) 
+    {
+        delay(100);
+        Serial.print(".");
+    }
+   
+    Serial.println();
+    Serial.print("Conectado com sucesso na rede ");
+    Serial.print(ssid);
+    Serial.println("IP obtido: ");
+    Serial.println(WiFi.localIP());
+}
+
+//Função: verifica o estado das conexões WiFI e ao broker MQTT. 
+//        Em caso de desconexão (qualquer uma das duas), a conexão
+//        é refeita.
+//Parâmetros: nenhum
+//Retorno: nenhum
+void VerificaConexoesWiFIEMQTT(void)
+{
+    if (!client.connected()) 
+        reconnectMQTT(); //se não há conexão com o Broker, a conexão é refeita
+     
+     reconectWiFi(); //se não há conexão com o WiFI, a conexão é refeita
 }
